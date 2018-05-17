53860b166138906cbea0d8c33ada28ca9c397215
Oleh <zholeg@ukr.net>
2018-05-17 10:34:31 +0300

    add test random

--- test/test.1.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-
-describe("random", function () {
-
-    it("Ищем истину", function () {
-        assert.equal(true === true, true);
-    });
-})
--- test/test.js
+++ test/test.js
@@ -1,39 +1,32 @@
 var assert = require('assert');
+var random = require('../random').default;
 
-// function equalRandom(min, max) {
-//     let err = false;
-//     for (let i = 0; i < 100; i++) {
-//         const val = random(min, max);
-//         if (val < min || val > max) {
-//             err = true;
-//             console.log(min, max, val);
-//         }
-//     }
-//     if (!err) {
-//         console.log('Тест прошел успешно!', min, max);
-//     }
-//     return err;
-// };
+function equalRandom(min, max) {
+    for (let i=0; i<1000; i++) {
 
-describe("random", function () {
+        const val = random(min, max);
+        const res = (val < min || val > max);
+        assert.equal(res, false, `Errors: min=${min}, max=${max}, return value=${val}, ${res}`);
+    }
+}
 
-    it("Ищем истину", function () {
-        assert.equal(true === true, true);
-    });
+describe("bouncing-balls", function () {
 
-    // it("Ищем истину", function () {
-    //     if (true === true) {
-    //         console.log('42');
-    //     } else {
-    //         console.log('Истины нет!')
-    //     }
-    // });
+    describe("Проверка подключения тестов", function () {
 
-    // it("Проверяем random", function () {
-    //     debugger;
-    //     equalRandom(-10, 10);
-    //     equalRandom(0, 0);
-    //     equalRandom(0, 10);
-    //     equalRandom(-10, 0);
-    // });
+        it("Ищем истину", function () {
+            assert.strictEqual(true, true, 'Истины нет!');
+        });
+
+        it("random", function () {
+            equalRandom(-10, 10);
+            equalRandom(0, 10);
+            equalRandom(-10, 0);
+            equalRandom(0, 0);
+            equalRandom(-1, -1);
+            equalRandom(1, 1);
+            equalRandom(1, 10);
+            equalRandom(-10, 1);
+        });
+    })
 })
