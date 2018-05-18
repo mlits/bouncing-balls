375048b26ade35b7f1225ce4b9d86cab0d74ccd0
kyrylo gerasymenko <ger.kirill@gmail.com>
2018-05-18 11:48:34 +0300

    fixes style issues and adds parcel to package,json to fix the build

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ const ctx = canvas.getContext('2d');
 
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
-let screenSize = {width, height};
+const screenSize = { width, height };
 
 window.addEventListener('resize', () => {
-	width = canvas.width = screenSize.width = window.innerWidth;
-	height = canvas.height = screenSize.height = window.innerHeight;
+  width = canvas.width = screenSize.width = window.innerWidth;
+  height = canvas.height = screenSize.height = window.innerHeight;
 });
 
 // define array to store balls
@@ -23,28 +23,28 @@ const balls = [];
 // define loop that keeps drawing the scene constantly
 
 function loop() {
-	ctx.fillStyle = 'rgba(0,0,0,1)';
-	ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = 'rgba(0,0,0,1)';
+  ctx.fillRect(0, 0, width, height);
 
-	while (balls.length < 25) {
-		const ball = new Ball(
-			random(0, width),
-			random(0, height),
-			random(-7, 7),
-			random(-7, 7),
-			`rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`,
-			random(10, 20)
-		);
-		balls.push(ball);
-	}
+  while (balls.length < 25) {
+    const ball = new Ball(
+      random(0, width),
+      random(0, height),
+      random(-7, 7),
+      random(-7, 7),
+      `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`,
+      random(10, 20)
+    );
+    balls.push(ball);
+  }
 
-	for (let i = 0; i < balls.length; i++) {
-		balls[i].draw(ctx);
-		balls[i].update(screenSize);
-		balls[i].collisionDetect(balls);
-	}
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw(ctx);
+    balls[i].update(screenSize);
+    balls[i].collisionDetect(balls);
+  }
 
-	requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 
