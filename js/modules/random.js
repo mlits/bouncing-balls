d888dc6d8e00a5de3b0698413e2fe246b85d493b
max <guitarockmax@gmail.com>
2018-05-20 23:23:19 +0300

    fix after eslint test

--- js/modules/random.js
+++ js/modules/random.js
@@ -1,15 +1,13 @@
 // function to generate random number
 export function random(min, max, exp) {
-  var num;
-  if ( exp != undefined ) {
+  let num;
+  if (exp !== undefined) {
     do {
       num = Math.floor(Math.random() * (max - min)) + min;
     }
-    while (num == 0);
-    return num;
-  }
-  else {
+    while (num === 0);
+  } else {
     num = Math.floor(Math.random() * (max - min)) + min;
-    return num;
   }
+  return num;
 }
